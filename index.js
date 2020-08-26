fa8b787cf5eac1a606773bda38b4c8c6346ed8a2
Jada Woody <JadaWoody@ENCMBP01.local>
2020-08-26 14:06:50 -0400

    going5

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('JS connected')
+// console.log('JS connected')
+// document.querySelector().onscroll=function(event)
+if(scroll in window){
+    alert('Hello there! Welcome to my Portfolio!')
+}
