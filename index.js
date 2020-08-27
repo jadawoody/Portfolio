b03c403bd97a172c066b316eec00358978f50c12
Jada Woody <JadaWoody@ENCMBP01.local>
2020-08-27 08:37:43 -0400

    going5

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // console.log('JS connected')
 // document.querySelector().onscroll=function(event)
-if(scroll in window){
-    alert('Hello there! Welcome to my Portfolio!')
+document.querySelector('.btn').onclick = getStats
+function getStats(){
+    axios
+        .get(`https://dog.ceo/api/breeds/image/random`)
+        .then((res)=> {
+            //console.log(res.data)
+            document.querySelector(".powGoesHere").innerHTML +=
+            
+            `<img src="${res.data.message}" />`
+
+        })
 }
+// document.querySelector('.emojiBtn').onclick = getEmoji
+// function getEmoji(){
+//     const emojikey = access_key=123412341234
+//     axios
+//     .get(`https://emoji-api.com/emojis/grinning-squinting-face`)
+//     .then((res)=>{
+//         document.querySelector("popGoesHere").innerHTML +=
+
+//         `<img src"${res.data.message}" />`
+//     })
+// }
