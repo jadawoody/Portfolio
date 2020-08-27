bd0c6a41d0dc3c630a1755da3a78b1f879603fb2
Jada Woody <JadaWoody@ENCMBP01.local>
2020-08-27 13:37:56 -0400

    going6

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,18 @@
 
 function getPortfolio(){
     axios
-    .get(`https://ironrest.herkuapp.com/mia-aug2020-webdevs`)
+    .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`)
     .then((res)=> {
         console.log(res.data)
         for (let i=0; i < res.data.length; i++) {
-            console.log(res.data[i])
+            //console.log(res.data[i])
         
-//document.querySelector("#classmates").innerHTML += `<li><a href=" ${res.data.website}"></a></a></li>`
+document.querySelector("#classmates").innerHTML += `<li><a href=" ${res.data[i].website}" target="blank">${res.data[i].first_name}</a></li>`
 
         }
     })
     
 }
 getPortfolio()
-console.log("test")
 
-document.querySelector('#classmates').onclick = getPortfolio()
+// document.querySelector('#classmates').onclick = getPortfolio()
